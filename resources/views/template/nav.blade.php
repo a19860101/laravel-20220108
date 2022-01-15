c4ba2e4ba5e1bdcf61b85b3a6b8c4b42fafdb395
Zac <a19860101@gmail.com>
2022-01-15 15:31:06 +0800

    20220115

--- resources/views/template/nav.blade.php
+++ resources/views/template/nav.blade.php
@@ -1,3 +1,7 @@
+<?php
+    $categories = \App\Category::get();
+?>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{route('admin.index')}}">QWERTY</a>
@@ -5,7 +9,16 @@
             <span class="navbar-toggler-icon"></span>
             </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">所有商品</a>
+                </li>
+                @foreach($categories as $category)
+                <li class="nav-item">
+                    <a href="#" class="nav-link">{{$category->title}}</a>
+                </li>
+                @endforeach
+            </ul>
         </div>
     </div>
 </nav>
