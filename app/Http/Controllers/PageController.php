01f1df50691fec2bdf829c778eb7b24dc701b07e
Zac <a19860101@gmail.com>
2022-01-15 15:20:22 +0800

    20220115

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,7 +9,9 @@ class PageController extends Controller
 {
     //
     public function index(){
-        $products = Product::orderBy('created_at','DESC')->limit(4)->get();
-        return view('index',compact('products'));
+        $new_products = Product::orderBy('created_at','DESC')->limit(4)->get();
+        $sale_products = Product::orderBy('created_at','DESC')->where('sale','!=',null)->limit(4)->get();
+        return view('index',compact('new_products','sale_products'));
+
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
         <div class="col-12">
             <h2>最新商品</h2>
         </div>
-        @foreach ($products as $new)
+        @foreach ($new_products as $new)
         <div class="col-3">
             <img src="{{asset('images/'.$new->cover)}}" class="w-100">
             <h3>{{$new->title}}</h3>
@@ -20,6 +20,31 @@
             @else
             <div class="fw-bold">
                 NT.{{$new->price}}
+            </div>
+            @endif
+        </div>
+        @endforeach
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h2>特價商品</h2>
+        </div>
+        @foreach ($sale_products as $sale)
+        <div class="col-3">
+            <img src="{{asset('images/'.$sale->cover)}}" class="w-100">
+            <h3>{{$sale->title}}</h3>
+            @if($sale->sale)
+            <div class="text-secondary">
+                <del>NT.{{$sale->price}}</del>
+            </div>
+            <div class="text-danger fw-bold">
+                NT.{{$sale->sale}}
+            </div>
+            @else
+            <div class="fw-bold">
+                NT.{{$sale->price}}
             </div>
             @endif
         </div>
