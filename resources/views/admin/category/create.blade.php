a10da379fc2fc624aca9aef7bd27466f06b7895a
Zac <a19860101@gmail.com>
2022-01-15 12:08:16 +0800

    20220115

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -1,32 +1,42 @@
 @extends('admin.template.master')
 
 @section('main')
-    <div>
-        <form action="{{route('category.store')}}" method="post">
-            @csrf
-            <div>
-                <label for="title" class="form-label">分類標題</label>
-                <input type="text" name="title" class="form-control">
-            </div>
-            <div>
-                <label for="slug" class="form-label">分類英文標題</label>
-                <input type="text" name="slug" class="form-control">
-            </div>
-            <input type="submit" class="btn btn-primary" value="建立分類">
-        </form>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h2>分類管理</h2>
+            <hr>
+        </div>
+        <div class="col-7">
+            <h3>新增分類</h3>
+            <form action="{{route('category.store')}}" method="post">
+                @csrf
+                <div class="mb-3">
+                    <label for="title" class="form-label">分類標題</label>
+                    <input type="text" name="title" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="slug" class="form-label">分類英文標題</label>
+                    <input type="text" name="slug" class="form-control">
+                </div>
+                <input type="submit" class="btn btn-primary" value="建立分類">
+            </form>
+        </div>
+        <div class="col-5">
+            <h3>分類列表</h3>
+            <ul class="list-group">
+                @foreach ($datas as $data)
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    {{$data->title}}
+                    <form action="{{route('category.destroy',['category'=>$data->id])}}" method="post">
+                        @csrf
+                        @method('delete')
+                        <input type="submit" value="刪除" class="btn btn-danger" onclick="return confirm('確認刪除？')">
+                    </form>
+                </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
-    <div>
-        <ul class="list-group">
-            @foreach ($datas as $data)
-            <li class="list-group-item">
-                {{$data->title}}
-                <form action="{{route('category.destroy',['category'=>$data->id])}}" method="post">
-                    @csrf
-                    @method('delete')
-                    <input type="submit" value="刪除" class="btn btn-danger" onclick="return confirm('確認刪除？')">
-                </form>
-            </li>
-            @endforeach
-        </ul>
-    </div>
+</div>
 @endsection
--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -1,35 +1,42 @@
 @extends('admin.template.master')
 
 @section('main')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-8">
 
-    <h2>新增商品</h2>
-    <form action="{{route('product.store')}}" method="post" enctype="multipart/form-data">
-        @csrf
-        <div>
-            <label for="title">商品名稱</label>
-            <input type="text" name="title" id="title">
+            <h2>新增商品</h2>
+            <hr>
+            <form action="{{route('product.store')}}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="mb-3">
+                    <label class="form-label" for="title">商品名稱</label>
+                    <input type="text" name="title" id="title" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="category_id">分類</label>
+                    <select name="category_id" id="category_id" class="form-select">
+                        @foreach ($categories as $item)
+                        <option value="{{$item->id}}">{{$item->title}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="price">售價</label>
+                    <input type="text" name="price" id="price" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="cover">商品圖片</label>
+                    <input type="file" name="cover">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="description">商品敘述</label>
+                    <textarea name="description" id="description" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+                <input type="submit" value="新增商品" class="btn btn-primary">
+                <input type="button" value="取消" onclick="history.back()" class="btn btn-danger">
+            </form>
         </div>
-        <div>
-            <label for="category_id">分類</label>
-            <select name="category_id" id="category_id">
-                @foreach ($categories as $item)
-                <option value="{{$item->id}}">{{$item->title}}</option>
-                @endforeach
-            </select>
-        </div>
-        <div>
-            <label for="price">售價</label>
-            <input type="text" name="price" id="price">
-        </div>
-        <div>
-            <label for="cover">商品圖片</label>
-            <input type="file" name="cover">
-        </div>
-        <div>
-            <label for="description">商品敘述</label>
-            <textarea name="description" id="description" cols="30" rows="10"></textarea>
-        </div>
-        <input type="submit" value="新增商品">
-        <input type="button" value="取消" onclick="history.back()">
-    </form>
+    </div>
+</div>
 @endsection
